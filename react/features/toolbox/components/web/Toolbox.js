e42ca646be738d5258eb41e6e9b3372b81357a3c
dastnbek <dostonbek.oripjonov@mail.ru>
2020-08-17 14:59:50 +0500

    Show Live and Rec icons when Live stream started and hide start recording button from setting. Show it when live stream turned off

--- react/features/toolbox/components/web/Toolbox.js
+++ react/features/toolbox/components/web/Toolbox.js
@@ -1,6 +1,11 @@
 // @flow
 
 import React, { Component } from 'react';
+import logger from './logger';
+
+import { getActiveSession } from '../../../recording/functions'
+import { JitsiRecordingConstants } from '../../../base/lib-jitsi-meet'
+
 import {
     ACTION_SHORTCUT_TRIGGERED,
     createShortcutEvent,
@@ -1032,10 +1037,15 @@ class Toolbox extends Component<Props, State> {
             _fullScreen,
             _screensharing,
             _sharingVideo,
+            _isLiveStreamRunning,
             t
         } = this.props;
 
-        return [
+        let arr = [];
+
+        
+
+        arr = [
             this._isProfileVisible()
                 && <OverflowMenuProfileItem
                     key = 'profile'
@@ -1053,9 +1063,6 @@ class Toolbox extends Component<Props, State> {
                     text = { _fullScreen ? t('toolbar.exitFullScreen') : t('toolbar.enterFullScreen') } />,
             <LiveStreamButton
                 key = 'livestreaming'
-                showLabel = { true } />,
-            <RecordButton
-                key = 'record'
                 showLabel = { true } />,
             this._shouldShowButton('sharedvideo')
                 && <OverflowMenuItem
@@ -1126,6 +1133,15 @@ class Toolbox extends Component<Props, State> {
                     key = 'help'
                     showLabel = { true } />
         ];
+
+
+        if(_isLiveStreamRunning === false ){
+            arr.splice(4, 0, <RecordButton key = 'record' showLabel = { true } /> )
+        }else {
+            arr.splice(4, 1)
+        }
+
+        return arr;
     }
 
     /**
@@ -1511,7 +1527,9 @@ function _mapStateToProps(state) {
             || sharedVideoStatus === 'pause',
         _visible: isToolboxVisible(state),
         _visibleButtons: equals(visibleButtons, buttons) ? visibleButtons : buttons,
-        _isModerator : isModerator
+        _isModerator : isModerator,
+        _isLiveStreamRunning: Boolean(
+            getActiveSession(state, JitsiRecordingConstants.mode.STREAM)),
     };
 }
 
