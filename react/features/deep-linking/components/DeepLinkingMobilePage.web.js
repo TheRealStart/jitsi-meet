5e7ec2fd07a68c44a858d9f33f65998b91a0b9d7
dastnbek <dostonbek.oripjonov@mail.ru>
2020-09-09 21:43:24 +0500

    Commented joining meeting with app

--- react/features/deep-linking/components/DeepLinkingMobilePage.web.js
+++ react/features/deep-linking/components/DeepLinkingMobilePage.web.js
@@ -134,7 +134,7 @@ class DeepLinkingMobilePage extends Component<Props> {
                     <p className = { `${_SNS}__text` }>
                         { t(`${_TNS}.appNotInstalled`, { app: NATIVE_APP_NAME }) }
                     </p>
-                    <p className = { `${_SNS}__text` }>
+                    {/* <p className = { `${_SNS}__text` }>
                         { t(`${_TNS}.ifHaveApp`) }
                     </p>
                     <a
@@ -146,7 +146,7 @@ class DeepLinkingMobilePage extends Component<Props> {
                         <button className = { `${_SNS}__button ${_SNS}__button_primary` }>
                             { t(`${_TNS}.joinInApp`) }
                         </button>
-                    </a>
+                    </a> */}
                     <p className = { `${_SNS}__text` }>
                         { t(`${_TNS}.ifDoNotHaveApp`) }
                     </p>
