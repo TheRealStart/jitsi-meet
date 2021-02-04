af082eb3427fea24709c0efe6a2e3462f56a103c
Ulugbek Nazarov <ulugbeknazarov15@gmail.com>
2021-02-04 20:36:17 +0500

    Added mouse click simulation on local video container when whiteboard is opened

--- react/features/etherpad/components/SharedDocumentButton.js
+++ react/features/etherpad/components/SharedDocumentButton.js
@@ -10,6 +10,68 @@ import { connect } from '../../base/redux';
 import { AbstractButton, type AbstractButtonProps } from '../../base/toolbox/components';
 import { toggleDocument } from '../actions';
 
+/**
+ * The function simulate simulates different types of events like
+ * a click event on a specific html element on the page
+ * @param {*} element - html element
+ * @param {*} eventName - event like 'click'
+ */
+function simulate(element, eventName)
+{
+    var options = extend(defaultOptions, arguments[2] || {});
+    var oEvent, eventType = null;
+    for (var name in eventMatchers)
+    {
+        if (eventMatchers[name].test(eventName)) { eventType = name; break; }
+    }
+    if (!eventType)
+        throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported');
+    if (document.createEvent)
+    {
+        oEvent = document.createEvent(eventType);
+        if (eventType == 'HTMLEvents')
+        {
+            oEvent.initEvent(eventName, options.bubbles, options.cancelable);
+        }
+        else
+        {
+            oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView,
+            options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
+            options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, element);
+        }
+        element.dispatchEvent(oEvent);
+    }
+    else
+    {
+        options.clientX = options.pointerX;
+        options.clientY = options.pointerY;
+        var evt = document.createEventObject();
+        oEvent = extend(evt, options);
+        element.fireEvent('on' + eventName, oEvent);
+    }
+    return element;
+}
+function extend(destination, source) {
+    for (var property in source)
+      destination[property] = source[property];
+    return destination;
+}
+var eventMatchers = {
+    'HTMLEvents': /^(?:load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll)$/,
+    'MouseEvents': /^(?:click|dblclick|mouse(?:down|up|over|move|out))$/
+}
+var defaultOptions = {
+    pointerX: 0,
+    pointerY: 0,
+    button: 0,
+    ctrlKey: false,
+    altKey: false,
+    shiftKey: false,
+    metaKey: false,
+    bubbles: true,
+    cancelable: true
+}
+
 
 type Props = AbstractButtonProps & {
 
@@ -47,6 +109,13 @@ class SharedDocumentButton extends AbstractButton<Props, *> {
             {
                 enable: !this.props._editing
             }));
+
+        if (!document.getElementById('localVideoContainer').classList.contains('videoContainerFocused') && !this.props._editing) {
+            simulate(document.getElementById("localVideo_container"), "click");
+        } else if (document.getElementById('localVideoContainer').classList.contains('videoContainerFocused') && this.props._editing) {
+            simulate(document.getElementById("localVideo_container"), "click");
+        }
+        
         this.props.dispatch(toggleDocument());
         
         if((!this.props._screensharing && !this.props._editing)){
